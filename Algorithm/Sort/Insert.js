d96e63a7a989248440303e762f37ec36abf82d15
XJawher <lplee7830@gmail.com>
2021-03-19 22:53:44 +0800

    insert algorithm

--- Algorithm/Sort/Insert.js
+++ Algorithm/Sort/Insert.js
@@ -8,14 +8,26 @@
  * 1 从第一个元素开始，就默认这个元素是排好序的了
  * 2 取出下一个元素，在已经排序的元素中从后向前扫描
  * 3 如果该元素，大于新元素，将该元素移动到下一个位置
+1. 从第一个元素开始，元素被认为是已经被排序。
+2. 取出下一个元素，在已经排序的元素中从后向前扫描。
+3. 如果该元素（已排序）大于新元素，将该元素移动到下一个位置。
+4. 重复步骤 3 ，直到找到已经排序的元素小于或者等于新元素的位置。
+5. 将新元素插入到该位置后。
+6. 重复步骤 2~5
  */
-function insert (params, len = params.length - 1) {
+function insert (params) {
     // 默认的第一个元素是 数组的最后一个元素。
-    console.log(params, params[len])
-    for (let index = 0; index < params.length; index++) {
-        const element = params[index];
-
+    for (let i = 0; i < params.length; i++) {
+        for (let j = i; j > 0; j--) {
+            if (params[j] > params[j - 1]) {
+                console.log(params[i], params[j]);
+                const tmp = params[j]
+                params[j] = params[j - 1]
+                params[j - 1] = tmp
+            }
+        }
     }
+    return params;
 }
 
-insert([1, 3, 4, 5], 3);
+insert([19, 3, 33, 2, 31, 43, 52, 45, 64]);
