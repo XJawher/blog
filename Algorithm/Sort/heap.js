da55b96f6f49ad722a7b4018d2b6cafc4e1e9306
XJawher <lplee7830@gmail.com>
2021-03-29 20:19:26 +0800

    heap sort algorithm

---
+++ Algorithm/Sort/heap.js
@@ -0,0 +1,16 @@
+/**
+ * @description 堆排序
+ * @author lipc
+ * @date 29/03/2021
+ * 1 构造堆，一般降序是大顶堆，升序是小顶堆
+ * 2 交换第一和末尾的值，继续在 0 ~ n-1 个值中构造堆
+ * 3 重复 1 2 直到数组长度是 1 。
+ * @param {*} params
+ */
+function heap (params) {
+
+}
+
+function heapFactory (params) {
+
+}
